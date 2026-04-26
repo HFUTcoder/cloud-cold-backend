d56ae52f9a81f342ab439fb850118541e0105e26
shenchen <7906208+shenchen0416@user.noreply.gitee.com>
2026-04-26 17:08:12 +0800

    milvus接入-20260426

--- src/main/java/com/shenchen/cloudcoldagent/controller/FileController.java
+++ src/main/java/com/shenchen/cloudcoldagent/controller/FileController.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriUtils;
-
-import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/files")
